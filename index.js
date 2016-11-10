d2f4f1ca3ade2572a61ee286d82920de3b9a50d4
Brian Hudson <brianjasonhudson@gmail.com>
2016-11-10 14:19:16 -0600

    Added passport and user auth

--- index.js
+++ index.js
@@ -2,14 +2,80 @@ const express = require("express"),
   app = express(),
   {json} = require("body-parser"),
   cors = require("cors"),
-  port = 8700,
+  session = require("express-session"),
+  passport = require("passport"),
+  {Strategy: FacebookStrategy} = require("passport-facebook"),
+  config = require("./config.js")
+  port = 8800,
   mongoose = require("mongoose"),
-  mongoUri = "mongodb://localhost:27017/ecommerce";
+  mongoUri = "mongodb://localhost:27017/ecommerce",
+  User = require("./features/users/User");
 
+app.use(session({secret: config.mySecrets.secret}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cors());
 app.use(json());
 app.use(express.static(`${__dirname}/public`));
 
+passport.use(new FacebookStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.secret,
+    callbackURL: config.facebook.cbUrl
+  },
+  function(token, refreshToken, profile, done) {
+    process.nextTick(function() {
+      console.log(profile)
+      User.findOne({ 'facebook_id': profile.id }, function(err, user) {
+        if (err) {
+          return done(err);
+        }
+        if (user) {
+          return done(null, user); // user found, return that user
+        } else {
+          var newUser = new User();
+            newUser.facebook_id = profile.id;
+            newUser.name  = profile.displayName;
+            newUser.last = profile.name.familyName;
+            if (profile.email) newUser.email = profile.email[0].value;
+
+            newUser.save(function(err) {
+                if (err)
+                    throw err;
+
+                // if successful, return the new user
+                return done(null, profile);
+
+            });
+        }
+      });
+    });
+
+  }));
+
+app.get("/auth/facebook", passport.authenticate("facebook"));
+
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    successRedirect: "http://localhost:8800/#/cart",
+    failureRedirect: 'http://localhost:8800/#/login'
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+app.get("/user", (req, res) => {
+  res.send(req.user);
+});
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('http://localhost:8800/#/');
+});
 
 mongoose.connect(mongoUri);
 mongoose.connection.once("open", () => console.log(`Connected to MongoDB at ${mongoUri}`));
